0da4a6c21b08ddcdec65c1fb393661ca152fddb7
Thegajout <gjuliens10@gmail.com>
2022-08-30 14:27:19 +0100

    refactor json server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiURL: 'http://localhost:3000/repos',
-  statsApiUrl: 'https://localhost:3000/repos/OWNER/REPO/stats/commit_activity'
+  apiURL: 'http://localhost:3000/projects',
+  statsApiUrl: 'http://localhost:3000/repos',
 };
 
 /*
